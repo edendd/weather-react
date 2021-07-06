c54aded78a15407d1f379c2066be1449da9200a8
edenhailu333@gmail.com <EDENhailu21>
2021-07-06 09:11:20 -0500

    changesMade

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Weather from "./Weather"
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
+    <div className="container">
       <header className="App-header">
-        <h1>Weather Search Engine</h1>
+        <h1>Weather-App</h1>
         <Weather/>
       </header>
+      <footer>
+        Coded by Eden D
+        <a href="https://github.com/edendd/weather-react">
+          {" "}
+          Open-sourced on Github
+        </a>
+      </footer>
+
+    </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,57 +1,20 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React  from "react";
+//import axios from "axios";
+import "./Weather.css";
 
-export default function Search(prop) {
-  let [city, setCity] = useState("");
-  let [Weather, setWeather] = useState("");
+export default function Weather() {
+  
+ return(
+   
+      <div className="Weather">
+        <h1>New York</h1>
+        <ul>
+          <li>Wednesday 7:00 PM</li>
+          <li>Mostly Cloudy</li>
 
-  function showTemperature(response) {
-    setWeather(
-      <ul>
-        <li className="lichild">
-          Temperature:{Math.round(response.data.main.temp)}℃
-        </li>
-
-        <li className="lichild">
-          Description: {response.data.weather[0].description}
-          <br />
-        </li>
-        <li className="lichild">Humidity: {response.data.humidity}%</li>
-        <li className="lichild">Wind: {response.data.wind.speed}km/h</li>
-        <li className="lichild">
-          <img
-            src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
-            alt={response.data.weather[0].description}
-          />
-        </li>
-      </ul>
-    );
+        </ul>
+      </div>
+ )  
   }
 
-  function handleSearch(event) {
-    event.preventDefault();
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=977e7cf2f6ebb86ab22f897eca8c19de&units=metric`;
-    axios.get(apiUrl).then(showTemperature);
-  }
 
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
-  return (
-      <div>
-    <form onSubmit={handleSearch} className = "search">
-      <input
-        type="search"
-        placeholder="Enter a city..."
-        onChange={updateCity}
-      />{" "}
-      <input type="submit" value="Search" />
-      <div>{Weather}</div>
-    </form>
-    
-    <a href= "https://github.com/edendd/weather-react">Git-hub</a>
-    </div>
-    
-  );
-}
